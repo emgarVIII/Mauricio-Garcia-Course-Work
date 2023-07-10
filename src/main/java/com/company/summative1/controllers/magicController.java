842c76ad3d99b4c7dff437f5f184cf4c1a3cc56f
Santiago F <santiferuseche@gmail.com>
2023-07-09 21:26:20 -0500

    added comments

--- src/main/java/com/company/summative1/controllers/magicController.java
+++ src/main/java/com/company/summative1/controllers/magicController.java
@@ -14,6 +14,7 @@ public class magicController {
     private List<String> AnswerPrompts = new ArrayList<>();
     private static int currId = 0;
 
+    //Controller response prompt data
     public magicController(){
         AnswerPrompts.add("Yes");
         AnswerPrompts.add("Maybe?");
@@ -25,22 +26,34 @@ public class magicController {
         AnswerPrompts.add("You better not");
         AnswerPrompts.add("Hard no.");
         AnswerPrompts.add("Go ahead!");
+        AnswerPrompts.add("You do you :)");
+        AnswerPrompts.add("DO IT!");
     }
 
+    /*
+    The Magic 8 Ball API provides a magic '8 ball' service
+    URI: /magic
+    Method: POST
+    Request Body: Question (Answer Module)
+    Response Body: Answer (Answer Module)
+    The POST request for the Magic 8 Ball endpoint should include a user-provided question.
+
+    NOTE: Typically we would add validation to ensure a valid request.
+     */
     @RequestMapping(value = "/magic", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
-    public Answer getRandomAnswer (@RequestBody @Valid Answer question){
-        //Answer response = new Answer();
-
+    public Answer getRandomAnswer (@RequestBody @Valid Answer Question){
+        // Get a random index from the answer prompts list
         Random random = new Random();
         int index = random.nextInt(AnswerPrompts.size());
 
-        question.setId(currId++);
-        //response.setQuestion(question);
-        //response.setQuestion(question.getQuestion());
-        question.setAnswer(AnswerPrompts.get(index));
+        // Assign a unique ID to the question
+        Question.setId(currId++);
 
-        return question;
+        // Set the answer of the question
+        Question.setAnswer(AnswerPrompts.get(index));
+
+        return Question;
     }
 
 }
